e9ea1399f3b9c133c1e72dbd1cfa7451fc81f993
huynhduc43 <thanhduc4320@gmail.com>
2022-12-04 16:42:28 +0700

    feat: load .dll file to create menu

--- BatchRename/MainWindow.xaml.cs
+++ BatchRename/MainWindow.xaml.cs
@@ -19,110 +19,190 @@ using Microsoft.WindowsAPICodePack.Dialogs;
 using Path = System.IO.Path;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Diagnostics;
+using IRuleLib;
+using System.Reflection;
+using BatchRenameAddPrefixRuleLib;
 
 namespace BatchRename
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-        public class Object : INotifyPropertyChanged
-        {
-            public string name;
-            public string dir;
-            public string extension;
-            public string Name
-            {
-                get => name; set{
-                    name = value;
-                    RaiseEvent();
-                }
-            }
-            public string Extension
-            {
-                get => extension; set{
-                    extension = value;
-                    RaiseEvent();
-                }
-            }
-            public string Dir
-            {
-                get => dir; set{
-                    dir = value;
-                    RaiseEvent();
-                }
-            }
-            public event PropertyChangedEventHandler PropertyChanged;
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+		public class Object : INotifyPropertyChanged
+		{
+			public string name;
+			public string dir;
+			public string extension;
+			public string Name
+			{
+				get => name; set
+				{
+					name = value;
+					RaiseEvent();
+				}
+			}
+			public string Extension
+			{
+				get => extension; set
+				{
+					extension = value;
+					RaiseEvent();
+				}
+			}
+			public string Dir
+			{
+				get => dir; set
+				{
+					dir = value;
+					RaiseEvent();
+				}
+			}
+			public event PropertyChangedEventHandler PropertyChanged;
 
-            void RaiseEvent([CallerMemberName] string propertyName = ""){
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-        BindingList<Object> Objects = new BindingList<Object>();
+			void RaiseEvent([CallerMemberName] string propertyName = "")
+			{
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
 
-        private void addFileButton_Click(object sender, RoutedEventArgs e)
-        {
-            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
-            dialog.Multiselect = true;
-            dialog.IsFolderPicker = false;
+		ObservableCollection<Object> Objects = new ObservableCollection<Object>();
+		List<IRule> _activeRules = new List<IRule>();
+		List<IRule> _rules = new List<IRule>();
 
-            if (Objects.Count != 0 && Objects[0].Extension == "")
-                Objects.Clear();
-            if (dialog.ShowDialog() == CommonFileDialogResult.Ok){
-                foreach (string sFileName in dialog.FileNames){
-                    Objects.Add(new Object { Name = Path.GetFileName(sFileName), Dir = Path.GetDirectoryName(sFileName) + "\\", Extension = Path.GetExtension(sFileName) });
-                    for (int i = 0; i < Objects.Count; i++){
-                        for (int j = i + 1; j < Objects.Count; j++){
-                            if (Objects[i].Name == Objects[j].Name)
-                                Objects.Remove(Objects[j]);
-                        }
-                    }
+		private void addFileButton_Click(object sender, RoutedEventArgs e)
+		{
+			CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+			dialog.Multiselect = true;
+			dialog.IsFolderPicker = false;
 
-                    sourceListView.Items.Clear();
-                    foreach (Object obj in Objects){
-                        sourceListView.Items.Add(obj);
-                    }
-                }
-            }
-        }
-        private void addFolderButton_Click(object sender, RoutedEventArgs e)
-        {
-            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
-            dialog.Multiselect = true;
-            dialog.IsFolderPicker = true;
+			if (Objects.Count != 0 && Objects[0].Extension == "")
+				Objects.Clear();
+			if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+			{
+				foreach (string sFileName in dialog.FileNames)
+				{
+					Objects.Add(new Object { Name = Path.GetFileName(sFileName), Dir = Path.GetDirectoryName(sFileName) + "\\", Extension = Path.GetExtension(sFileName) });
+					for (int i = 0; i < Objects.Count; i++)
+					{
+						for (int j = i + 1; j < Objects.Count; j++)
+						{
+							if (Objects[i].Name == Objects[j].Name)
+								Objects.Remove(Objects[j]);
+						}
+					}
 
-            if (Objects.Count != 0 && Objects[0].Extension != "")
-                Objects.Clear();
-            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
-            {
-                foreach (string sFileName in dialog.FileNames)
-                {
-                    Objects.Add(new Object { Name = Path.GetFileName(sFileName), Dir = Path.GetDirectoryName(sFileName) + "\\", Extension = Path.GetExtension(sFileName) });
-                    for (int i = 0; i < Objects.Count; i++)
-                    {
-                        for (int j = i + 1; j < Objects.Count; j++)
-                        {
-                            if (Objects[i].Name == Objects[j].Name)
-                                Objects.Remove(Objects[j]);
-                        }
-                    }
+					sourceListView.ItemsSource = Objects;
+					previewListView.ItemsSource = Objects;
+				}
+			}
+		}
+		private void addFolderButton_Click(object sender, RoutedEventArgs e)
+		{
+			CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+			dialog.Multiselect = true;
+			dialog.IsFolderPicker = true;
 
-                    sourceListView.Items.Clear();
-                    foreach (Object obj in Objects){
-                        sourceListView.Items.Add(obj);
-                    }
-                }
-            }
-        }
+			if (Objects.Count != 0 && Objects[0].Extension != "")
+				Objects.Clear();
+			if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+			{
+				foreach (string sFileName in dialog.FileNames)
+				{
+					Objects.Add(new Object { Name = Path.GetFileName(sFileName), Dir = Path.GetDirectoryName(sFileName) + "\\", Extension = Path.GetExtension(sFileName) });
+					for (int i = 0; i < Objects.Count; i++)
+					{
+						for (int j = i + 1; j < Objects.Count; j++)
+						{
+							if (Objects[i].Name == Objects[j].Name)
+								Objects.Remove(Objects[j]);
+						}
+					}
 
-        private void playBatchButton_Click(object sender, RoutedEventArgs e)
-        {
-             
-        }
-    }
+					sourceListView.Items.Clear();
+					foreach (Object obj in Objects)
+					{
+						sourceListView.Items.Add(obj);
+					}
+				}
+			}
+		}
+
+		private void playBatchButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void addMethod_Click(object sender, RoutedEventArgs e)
+		{
+			ContextMenu? cm = FindResource("renamingRulesContextMenu") as ContextMenu;
+			cm!.PlacementTarget = sender as Button;
+			cm.IsOpen = true;
+		}
+
+		private void menuItemRenamingRulesContextMenu_Click(object sender, RoutedEventArgs e)
+		{
+			var menuItem = sender as MenuItem;
+
+			Console.WriteLine(menuItem.Header);
+
+			if (menuItem != null)
+			{
+				_activeRules.Add(new AddPrefixRule() { Prefix = "CV" });
+
+				var converter = (PreviewRenameConverter)FindResource("PreviewRenameConverter");
+				converter.Rules = _activeRules;
+
+				var temp = new ObservableCollection<Object>();
+
+
+
+				foreach (var file in Objects)
+				{
+					temp.Add(file);
+				}
+
+				Objects = temp;
+				previewListView.ItemsSource = Objects;
+			}
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			var exeFolder = AppDomain.CurrentDomain.BaseDirectory;
+			var folderInfo = new DirectoryInfo(exeFolder);
+			var dllFiles = folderInfo.GetFiles("*RuleLib.dll");
+
+			foreach (var dll in dllFiles)
+			{
+				Assembly assembly = Assembly.LoadFrom(dll.FullName);
+				Type[] types = assembly.GetTypes();
+
+				foreach (Type type in types)
+				{
+					if (type.IsClass)
+					{
+						if (typeof(IRule).IsAssignableFrom(type))
+						{
+							_rules.Add((Activator.CreateInstance(type) as IRule)!);
+						}
+					}
+				}
+			}
+
+			foreach (IRule rule in _rules)
+			{
+				ContextMenu? contextMenu = FindResource("renamingRulesContextMenu") as ContextMenu;
+				var menuItem = new MenuItem();
+				menuItem.Header = rule.Label;
+				contextMenu!.Items.Add(menuItem);
+			}
+		}
+	}
 }
---
+++ IRule/IRule.cs
@@ -0,0 +1,13 @@
+﻿namespace IRuleLib
+{
+	public interface IRule : ICloneable
+	{
+		string Name { get; }
+
+		string Label { get; }
+
+		string Rename(string origin);
+
+		IRule Parse(string data);
+	}
+}

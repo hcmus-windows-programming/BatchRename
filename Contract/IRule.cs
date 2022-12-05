6f13baaafc233646506dc4c2b4562c2b2f4cba9f
MarsonVo2002 <114413325+MarsonVo2002@users.noreply.github.com>
2022-12-05 20:41:35 +0700

    Add files via upload
    Cái này là file contract(interface IRule). Mọi người khi cài đặt các luật nhớ lấy file Contract.dll bỏ vào rồi cài đặt nha

---
+++ Contract/IRule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contract
+{
+   public interface IRule
+   {
+        string Rename(string name);
+        IRule Parse(string data);
+        string name { get; }
+   }
+}

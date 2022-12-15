6d376e7007d2e0a0a7a1a1cf8da79ff38963a3dd
MarsonVo2002 <114413325+MarsonVo2002@users.noreply.github.com>
2022-12-15 13:51:33 +0700

    Add files via upload

---
+++ Final version/Source code/BatchRename/EditPresetWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BatchRename
+{
+    /// <summary>
+    /// Interaction logic for EditPresetWindow.xaml
+    /// </summary>
+    public partial class EditPresetWindow : Window
+    {
+        public string newPreset { get; set; }
+        public string[] newRules { get; set; }
+        public EditPresetWindow(string PresetPath)
+        {
+            InitializeComponent();
+            newPreset = PresetPath;
+            DataContext = newPreset;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            textbox.Text = File.ReadAllText(newPreset);
+        }
+
+        private void Okbutton_Click(object sender, RoutedEventArgs e)
+        {
+            newRules = textbox.Text.Split('\n');
+            DialogResult = true;
+        }
+
+        private void Cancelbutton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}

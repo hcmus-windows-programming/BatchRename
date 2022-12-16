6681768aba2e44b025e7e51374d79b2231ad4188
unknown <lyhai1502@gmail.com>
2022-12-16 13:50:53 +0700

    Update master

---
+++ CreateDLLFiles/AddCurrentMonth/AddCurrentMonth.cs
@@ -0,0 +1,33 @@
+﻿using Contract;
+using System.ComponentModel;
+
+namespace AddCurrentMonth
+{
+    public class AddCurrentMonth : IRule
+    {
+        public string Name => "AddCurrentMonth";
+
+        public string Label => "Add current month";
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        public IRule? Parse(string data)
+        {
+            return new AddCurrentMonth();
+        }
+
+        public string Rename(string origin)
+        {
+            var month = DateTime.Now.Month;
+            var year = DateTime.Now.Year % 100 ;
+
+            string result = $"{year}{month} {origin}";
+            return result;
+        }
+    }
+}
---
+++ CreateDLLFiles/ReplaceCertainChars/ReplaceCertainChars.cs
@@ -0,0 +1,76 @@
+﻿using Contract;
+using System.ComponentModel;
+using System.Text;
+
+namespace ReplaceCertainChars
+{
+    public class ReplaceCertainChars : IRule
+    {
+        public List<string> CertainChars { get; set; }
+        public string Replacement { get; set; }
+
+        public string Name => "ReplaceCertainChars";
+
+        public string Label => "Replace certain chars";
+        public ReplaceCertainChars()
+        {
+            CertainChars = new List<string>();
+            Replacement = "";
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        public IRule? Parse(string line)
+        {
+            var tokens = line.Split(new string[] { " " },
+                StringSplitOptions.None);
+
+            var data = tokens[1];
+            var attributes = data.Split(new string[] { "," },
+                StringSplitOptions.None);
+
+            var pairs1 = attributes[0].Split(new string[] { "=" },
+                StringSplitOptions.None); 
+            var certains = pairs1[1];
+            var pairs2 = attributes[1].Split(new string[] { "=" },
+                StringSplitOptions.None);
+
+            var replace = pairs2[1];
+
+            var rule = new ReplaceCertainChars();
+
+            foreach (var c in certains)
+            {
+                rule.CertainChars.Add($"{c}");
+            }
+
+            rule.Replacement = replace;
+
+            return rule;
+        }
+
+        public string Rename(string origin)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (var c in origin)
+            {
+                if (CertainChars.Contains($"{c}"))
+                {
+                    builder.Append(Replacement);
+                }
+                else
+                {
+                    builder.Append(c);
+                }
+            }
+
+            string result = builder.ToString();
+            return result;
+        }
+    }
+}
